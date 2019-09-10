c683d5da31a8fe0a1e0b7af623a9b4590900c107
Eugeny Sobolenko <stereotype1@mail.ru>
2019-09-10 21:31:25 +0400

    add correct_answer_key test in game_question

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe GameQuestion, type: :model do
 
   # задаем локальную переменную game_question, доступную во всех тестах этого сценария
   # она будет создана на фабрике заново для каждого блока it, где она вызывается
-  let(:game_question) { FactoryGirl.create(:game_question, a: 2, b: 1, c: 4, d: 3) }
+  let(:game_question) { FactoryBot.create(:game_question, a: 2, b: 1, c: 4, d: 3) }
 
   # группа тестов на игровое состояние объекта вопроса
   context 'game status' do
@@ -20,9 +20,18 @@ RSpec.describe GameQuestion, type: :model do
                                             'd' => game_question.question.answer3})
     end
 
+    it 'correct .level & .text delegates' do
+      expect(game_question.text).to eq(game_question.question.text)
+      expect(game_question.level).to eq(game_question.question.level)
+    end
+
     it 'correct .answer_correct?' do
       # именно под буквой b в тесте мы спрятали указатель на верный ответ
       expect(game_question.answer_correct?('b')).to be_truthy
+    end
+
+    it 'correct .correct_answer_key' do
+      expect(game_question.correct_answer_key).to eq('b')
     end
   end
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,13 +11,13 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
 
   context 'validations check' do
-
     it { should validate_presence_of :text }
     it { should validate_presence_of :level }
-
     it { should validate_inclusion_of(:level).in_range(0..14) }
-
     it { should allow_value(14).for(:level) }
     it { should_not allow_value(15).for(:level) }
+
+    subject { Question.new(text: 'some', level: 0, answer1: '1', answer2: '1', answer3: '1', answer4: '1') }
+    it { should validate_uniqueness_of(:text) }
   end
 end
