2f0bcdd74416ffe1d77f04b82d0c09fc0fe87772
Eugeny Sobolenko <stereotype1@mail.ru>
2019-09-23 22:04:34 +0400

    test for help friend_call

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -33,6 +33,25 @@ RSpec.describe GameQuestion, type: :model do
     it 'correct .correct_answer_key' do
       expect(game_question.correct_answer_key).to eq('b')
     end
+
+    it 'correct .help_hash' do
+      # на фабрике у нас изначально хэш пустой
+      expect(game_question.help_hash).to eq({})
+
+      # добавляем пару ключей
+      game_question.help_hash[:some_key1] = ['a','c']
+      game_question.help_hash['some_key2'] = { 'a' => 35, 'b' => 43 }
+      game_question.help_hash[:some_key3] = 'Answer correct'
+
+      # сохраняем модель и ожидаем сохранения хорошего
+      expect(game_question.save).to be_truthy
+
+      # загрузим этот же вопрос из базы для чистоты эксперимента
+      gq = GameQuestion.find(game_question.id)
+
+      # проверяем новые значение хэша
+      expect(gq.help_hash).to eq({some_key1: ['a', 'c'], 'some_key2' => {'a' => 35, 'b' => 43}, some_key3: 'Answer correct'})
+    end
   end
 
   # help_hash у нас имеет такой формат:
@@ -46,13 +65,32 @@ RSpec.describe GameQuestion, type: :model do
   context 'user helpers' do
     it 'correct audience_help' do
       expect(game_question.help_hash).not_to include(:audience_help)
-
       game_question.add_audience_help
-
       expect(game_question.help_hash).to include(:audience_help)
 
       ah = game_question.help_hash[:audience_help]
       expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+
+    it 'correct fifty_fifty' do
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+      game_question.add_fifty_fifty
+      expect(game_question.help_hash).to include(:fifty_fifty)
+
+      ff = game_question.help_hash[:fifty_fifty]
+      expect(ff).to include('b')
+      expect(ff.size).to eq 2
+    end
+
+    it 'correct friend_call' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+      game_question.add_friend_call
+      expect(game_question.help_hash).to include(:friend_call)
+
+      fc = game_question.help_hash[:friend_call]
+      expect(fc).to be_a(String)
+      expect(fc).to include('считает, что это вариант')
+      expect(fc).not_to be_empty
     end
   end
 end
